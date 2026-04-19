6ae5d5e3b0dcfa46628272330bba064b9d40090b
nile466 <urewanaru7890@gmail.com>
2026-04-20 01:48:50 +0530

    Update app.py
    fixed port configuration

--- app.py
+++ app.py
@@ -348,4 +348,8 @@ def reset_votes():
 # ── Entry point ───────────────────────────────────────────────────────────────
 if __name__ == "__main__":
     print("Voting system running at http://127.0.0.1:5000")
-    app.run(debug=True)
+    import os
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT",5000))
+        app.run(host="0.0.0.0", port=port)
+
